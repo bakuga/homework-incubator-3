6f90550d3e9f7fb8df7d52fbf995342a0397320e
CharricK <maximgancharik@mail.ru>
2025-01-21 14:16:25 +0300

    Initial commit

---
+++ src/s2-homeworks/hw01/friend-message/FriendMessage.tsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import s from './FriendMessage.module.css'
+import {MessageType} from "../HW1";
+import {MessagePropsType} from "../message/Message";
+
+type FriendMessagePropsType = {
+    message: MessageType;
+}
+
+// создать тип вместо any и отобразить приходящие данные
+const FriendMessage = ({message}: FriendMessagePropsType) => {
+    return (
+        <div
+            id={'hw1-friend-message-' + message.id}
+            className={s.friendMessage}
+        >
+            <div className={s.friendImageAndText}>
+                <img className={s.img}
+                    id={'hw1-friend-avatar-' + message.id}
+                    // создаёт студент
+
+                    //
+
+                    src={message.user.avatar}
+                />
+                <div className={s.friendText}>
+                    <div
+                        id={'hw1-friend-name-' + message.id}
+                        className={s.friendName}
+                    > {message.user.name}
+                        {/*создаёт студент*/}
+
+                        {/**/}
+                    </div>
+                    <pre
+                        id={'hw1-friend-text-' + message.id}
+                        className={s.friendMessageText}
+                    >
+                        {message.message.text}
+                        {/*создаёт студент*/}
+
+                        {/**/}
+                    </pre>
+                </div>
+            </div>
+            <div
+                id={'hw1-friend-time-' + message.id}
+                className={s.friendTime}
+            >
+                {message.message.time}
+                {/*создаёт студент*/}
+
+                {/**/}
+            </div>
+        </div>
+    )
+}
+
+export default FriendMessage
